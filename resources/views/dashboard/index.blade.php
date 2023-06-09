53d5ff3d8662b213c60cd965f6957f1a30704bd4
Vallory <carlvallory@gmail.com>
2023-06-09 15:19:45 -0400

    Dashboard 0.3 fix

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -44,6 +44,7 @@
                                 @foreach ($msgs as $key => $msg)
                                     <div class="mt-2 text-gray-600 dark:text-gray-400 text-sm" id="{{$msg->id}}">
                                         {{$msg->body}}
+                                        @dd($msg)
                                     </div>
                                 @endforeach
                             </div>
