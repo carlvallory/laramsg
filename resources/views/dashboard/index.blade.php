3f5825e27fe1befdfdbabfb0c7523fa1de576c7c
Vallory <carlvallory@gmail.com>
2023-06-09 15:18:20 -0400

    Dashboard 0.3

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -42,8 +42,8 @@
 
                             <div class="ml-12">
                                 @foreach ($msgs as $key => $msg)
-                                    <div class="mt-2 text-gray-600 dark:text-gray-400 text-sm">
-                                        @dd($msg)
+                                    <div class="mt-2 text-gray-600 dark:text-gray-400 text-sm" id="{{$msg->id}}">
+                                        {{$msg->body}}
                                     </div>
                                 @endforeach
                             </div>
