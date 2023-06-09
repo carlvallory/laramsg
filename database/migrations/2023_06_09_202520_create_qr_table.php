155eee0ddd921564911cc50b8545852e5a473080
Vallory <carlvallory@gmail.com>
2023-06-09 16:31:09 -0400

    Dashboard QR Text

--- database/migrations/2023_06_09_202520_create_qr_table.php
+++ database/migrations/2023_06_09_202520_create_qr_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
     {
         Schema::create('qr', function (Blueprint $table) {
             $table->id();
-            $table->string('qr_str');
+            $table->text('qr_str');
             $table->timestamps();
         });
     }
