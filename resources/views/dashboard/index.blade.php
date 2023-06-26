2ec4d32e14b5bd7d363389e93610064c3774ffaa
Vallory <carlvallory@gmail.com>
2023-06-26 16:40:27 -0400

    Chat css 1.4.3 change colors

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -95,7 +95,7 @@
             });
 
             $(document).ready(function(){
-                var colors = ["#2C2C2E","#2C2C2E"];                
+                var colors = ["#2C2C2E","#4E5462"];                
                 var rand = Math.floor(Math.random()*colors.length);           
                 document.querySelectorAll('.chat_box .chat_message_wrapper ul.chat_message > li a').forEach(element => { element.style.color = colors[rand]; });
             });
