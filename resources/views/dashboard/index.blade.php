1ceed56f05e97a443dbb9ef311af9cc37e361792
Vallory <carlvallory@gmail.com>
2023-06-26 09:18:24 -0400

    Chat css

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -23,74 +23,73 @@
         </style>
 
         <link rel="stylesheet" href="{{ asset('css/wa_app.css?v='.((int)(time()/60))) }}">
+
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
     </head>
     <body class="antialiased">
         <div class="container">
             <div>
-                <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
-
                 <aside id="sidebar_secondary" class="tabbed_sidebar ng-scope chat_sidebar">
-                <div class="popup-head">
-                    <div class="popup-head-left pull-left"><a design and developmenta title="Gurdeep Osahan (Web Designer)" target="_blank" href="https://web.facebook.com/iamgurdeeposahan">
-                    <img class="md-user-image" alt="Gurdeep Osahan (Web Designer)" title="Gurdeep Osahan (Web Designer)" src="https://bootdey.com/img/Content/avatar/avatar1.png" title="Gurdeep Osahan (Web Designer)" alt="Gurdeep Osahan (Web Designer)">
-                    <h1>GEN</h1><small><br> <span class="glyphicon glyphicon-briefcase" aria-hidden="true"></span> La Nacion</small></a></div>
-                    <div class="popup-head-right pull-right">
-                        <button class="chat-header-button" type="button"><i class="glyphicon glyphicon-facetime-video"></i></button>
-                        <button class="chat-header-button" type="button"><i class="glyphicon glyphicon-earphone"></i></button>
-                        <div class="btn-group gurdeepoushan">
-                        <button class="chat-header-button" data-toggle="dropdown" type="button" aria-expanded="false">
-                        <i class="glyphicon glyphicon-paperclip"></i> </button>
-                        <ul role="menu" class="dropdown-menu pull-right">
-                        <li><a href="#"><span class="glyphicon glyphicon-picture" aria-hidden="true"></span> Gallery</a></li>
-                        <li><a href="#"><span class="glyphicon glyphicon-camera" aria-hidden="true"></span> Photo</a></li>
-                        <li><a href="#"><span class="glyphicon glyphicon-facetime-video" aria-hidden="true"></span> Video</a></li>
-                        <li><a href="#"><span class="glyphicon glyphicon-headphones" aria-hidden="true"></span> Audio</a></li>
-                        <li><a href="#"><span class="glyphicon glyphicon-user" aria-hidden="true"></span> Contact</a></li>
-                        </ul>
-                        </div>
-                        <button data-widget="remove" id="removeClass" class="chat-header-button pull-right" type="button"><i class="glyphicon glyphicon-remove"></i></button>
-                    </div>
-                </div>
-
-                <div id="chat" class="chat_box_wrapper chat_box_small chat_box_active" style="opacity: 1; display: block; transform: translateX(0px);">
-                    <div class="chat_box touchscroll chat_box_colors_a">
-                    
-                        @foreach($msgs as $key => $msg)
-                            <div class="chat_message_wrapper">
-                                <div class="chat_user_avatar">
-                                    <a href="#" target="_blank">
-                                    <img alt="{{base64_decode($msg->msg_name)}}" title="{{base64_decode($msg->msg_name)}}" src="{{base64_decode($msg->msg_image)}}" class="md-user-image">
-                                    </a>
-                                </div>
+                    <div class="glow">
+                        <div class="frame">
+                            <div class="popup-head">
+                                <div class="popup-head-left pull-left">
                                 
-                                <ul class="chat_message" id="{{$msg->msg_id}}" data-from="{{$msg->msg_from}}">
-                                    <li>
-                                        <a> {{base64_decode($msg->msg_name)}} </a>
-                                        <p> {{base64_decode($msg->msg_body)}} </p>
-                                    </li>
-                                    {{-- <li>
-                                    <p> Lorem ipsum dolor sit amet.<span class="chat_message_time">13:38</span> </p>
-                                    </li> --}}
-                                </ul>
+                                    <h1>Whatsapp</h1>
 
-                                <input type="hidden" class="schedule_title" value="{{$msg->schedule->title}}" />
-
+                                </div>
+                                <div class="popup-head-right pull-right">
+                                    <div class="btn-group gurdeepoushan">
+                                        <button class="chat-header-button" data-toggle="dropdown" type="button" aria-expanded="false">
+                                        <i class="fa fa-ellipsis-h" aria-hidden="true"></i> </button>
+                                        <ul role="menu" class="dropdown-menu pull-right">
+                                            <li><a href="#"><span class="glyphicon glyphicon-user" aria-hidden="true"></span> Contact</a></li>
+                                        </ul>
+                                    </div>
+                                </div>
                             </div>
-                        @endforeach
-                    
+
+                            <div id="chat" class="chat_box_wrapper chat_box_small chat_box_active" style="opacity: 1; display: block; transform: translateX(0px);">
+                                <div class="chat_box touchscroll chat_box_colors_a">
+                                
+                                    @foreach($msgs as $key => $msg)
+                                        <div class="chat_message_wrapper">
+                                            <div class="chat_user_avatar">
+                                                <a href="#" target="_blank">
+                                                <img alt="{{base64_decode($msg->msg_name)}}" title="{{base64_decode($msg->msg_name)}}" src="{{base64_decode($msg->msg_image)}}" class="md-user-image">
+                                                </a>
+                                            </div>
+                                            
+                                            <ul class="chat_message" id="{{$msg->msg_id}}" data-from="{{$msg->msg_from}}">
+                                                <li>
+                                                    <a> {{base64_decode($msg->msg_name)}} </a>
+                                                    <p> {{base64_decode($msg->msg_body)}} </p>
+                                                </li>
+                                                {{-- <li>
+                                                <p> Lorem ipsum dolor sit amet.<span class="chat_message_time">13:38</span> </p>
+                                                </li> --}}
+                                            </ul>
+
+                                            <input type="hidden" class="schedule_title" value="{{$msg->schedule->title}}" />
+
+                                        </div>
+                                    @endforeach
+                                
+                                </div>
+                            </div>
+            
+            
+                        </div>
                     </div>
-                </div>
-            
-            
+                </aside>
             </div>
-            </div>
-            </div>
-            <script src="https://code.jquery.com/jquery-1.10.2.min.js"></script>
-            <script src="https://netdna.bootstrapcdn.com/bootstrap/3.3.2/js/bootstrap.min.js"></script>
-            <script type="text/javascript">
-                $(function(){
-                    $('#sidebar_secondary').addClass('popup-box-on');
-                });
-            </script>
+        </div>
+        <script src="https://code.jquery.com/jquery-1.10.2.min.js"></script>
+        <script src="https://netdna.bootstrapcdn.com/bootstrap/3.3.2/js/bootstrap.min.js"></script>
+        <script type="text/javascript">
+            $(function(){
+                $('#sidebar_secondary').addClass('popup-box-on');
+            });
+        </script>
     </body>
 </html>
