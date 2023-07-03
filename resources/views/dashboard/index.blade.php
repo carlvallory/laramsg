94c185a4af4ec2f05b812fb52bd1ed69eec174a7
Vallory <carlvallory@gmail.com>
2023-07-03 11:06:45 -0400

    Picture Profile Placeholder 6

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -59,7 +59,7 @@
                                         <div class="chat_message_wrapper">
                                             <div class="chat_user_avatar">
                                                 <a href="#" target="_blank">
-                                                <img alt="{{base64_decode($msg->msg_name)}}" title="{{base64_decode($msg->msg_name)}}" src="@if(!is_empty(base64_decode($msg->msg_image)) {{base64_decode($msg->msg_image)}} @else {{asset('images/default.svg')}} @endif" class="md-user-image">
+                                                <img alt="{{base64_decode($msg->msg_name)}}" title="{{base64_decode($msg->msg_name)}}" src="@if(!is_empty(base64_decode($msg->msg_image))) {{base64_decode($msg->msg_image)}} @else {{asset('images/default.svg')}} @endif" class="md-user-image">
                                                 </a>
                                             </div>
                                             
