467203a888ac3026a3b37320f6202a15632ddf3c
Vallory <carlvallory@gmail.com>
2023-07-07 16:09:37 -0400

    Login Auth 2.0.6

--- resources/views/dashboard/qrcode.blade.php
+++ resources/views/dashboard/qrcode.blade.php
@@ -1,7 +1,4 @@
 <div class="card">
-    <div class="card-header">
-        <h2>Simple QR Code</h2>
-    </div>
     <div class="card-body">
         {!! QrCode::size(300)->generate(base64_decode($qr->qr_str)) !!}
     </div>
