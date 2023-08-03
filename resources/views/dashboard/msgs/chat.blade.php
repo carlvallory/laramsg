9b217e37742f32fa2b666ed202032edc81f97cd6
Vallory <carlvallory@gmail.com>
2023-08-03 14:52:24 -0400

    Chat css 0.1.17

--- resources/views/dashboard/msgs/chat.blade.php
+++ resources/views/dashboard/msgs/chat.blade.php
@@ -10,6 +10,7 @@
 <link rel="stylesheet" href="{{ asset('css/bootstrap-checkbox.css') }}">
 
 <link rel="stylesheet" href="{{ asset('css/wa_web.css?v='.((int)(time()/60))) }}">
+<link rel="stylesheet" href="https://flatlogic.github.io/awesome-bootstrap-checkbox/bower_components/Font-Awesome/css/font-awesome.css">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 
 </head>
